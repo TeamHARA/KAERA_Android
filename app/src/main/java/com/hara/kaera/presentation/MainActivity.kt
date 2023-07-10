6f1f0626e8faca4bcf0ec7d21d365dfdb53dee70
IslandofDream <rhffldkt0517@naver.com>
2023-07-10 19:24:25 +0900

    [Feat] Timber 설정및 BuildConfing 활성화

--- app/src/main/java/com/hara/kaera/presentation/MainActivity.kt
+++ app/src/main/java/com/hara/kaera/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hara.kaera
+package com.hara.kaera.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hara.kaera.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
