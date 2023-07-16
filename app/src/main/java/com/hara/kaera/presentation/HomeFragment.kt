f1e6e291198642d0537ecb571eafa84249a3c348
Skylar <olfaminion0116@naver.com>
2023-07-16 22:13:28 +0900

    [Feat] 바텀 네비게이션 추가

---
+++ app/src/main/java/com/hara/kaera/presentation/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.hara.kaera.presentation
+
+import android.os.Bundle
+import android.view.View
+import com.hara.kaera.R
+import com.hara.kaera.databinding.FragmentHomeBinding
+import com.hara.kaera.presentation.base.BindingFragment
+
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
