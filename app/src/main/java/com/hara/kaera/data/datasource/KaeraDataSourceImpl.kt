48ce59640217c301977c818a43bbb604e6e91550
IslandofDream <rhffldkt0517@naver.com>
2023-07-29 00:26:49 +0900

    [Feat] UseCase 구현

--- app/src/main/java/com/hara/kaera/data/datasource/KaeraDataSourceImpl.kt
+++ app/src/main/java/com/hara/kaera/data/datasource/KaeraDataSourceImpl.kt
@@ -7,7 +7,8 @@ import javax.inject.Inject
 
 /*
 실제로 KaeraApi 기반으로 만들어진 레트로핏 객체를 구현하는 곳
-그래서 레트로핏객체를 주입받고 있다.
+그래서 레트로핏객체를 주입받고 있다. 추가적으로 flow를 사용하므로
+리턴타입으로 flow를 해주고 emit을 하는 형태로 리턴한다.
  */
 
 class KaeraDataSourceImpl @Inject constructor(
