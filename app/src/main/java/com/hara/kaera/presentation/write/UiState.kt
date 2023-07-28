e9f712f9239e521efd6bf0a8ed90f98e114157a5
IslandofDream <rhffldkt0517@naver.com>
2023-07-29 00:52:51 +0900

    [Feat] UiState기반 실제 적용 로직 구현

--- app/src/main/java/com/hara/kaera/presentation/write/UiState.kt
+++ app/src/main/java/com/hara/kaera/presentation/write/UiState.kt
@@ -10,7 +10,7 @@ Ui의 상태를 나타내는 오브젝트/ 클래스등을 미리 선언해두�
 sealed class UiState {
     object Loading : UiState()
     // 로딩이니까 담아줄꺼 없으므로 그냥 오브젝트
-    data class Success<T>(val data:T) : UiState()
+    data class Success<out T>(val data:T) : UiState()
     // 제네릭 타입으로 아무 타입이나 담을수 있도록 즉, 서버통신 결과가 여기에 담길것
     data class Error(val message:String) : UiState()
     // String으로 에러메시지 담도록 의도 T 타입해서 다양한 상호작용 가능
--- app/src/main/java/com/hara/kaera/presentation/write/WriteActivity.kt
+++ app/src/main/java/com/hara/kaera/presentation/write/WriteActivity.kt
@@ -2,13 +2,23 @@ package com.hara.kaera.presentation.write
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.hara.kaera.R
 import com.hara.kaera.databinding.ActivityWriteBinding
+import com.hara.kaera.domain.entity.TemplateTypesEntity
 import com.hara.kaera.presentation.base.BindingActivity
 import com.hara.kaera.presentation.util.onSingleClick
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
+@AndroidEntryPoint
 class WriteActivity : BindingActivity<ActivityWriteBinding>(R.layout.activity_write) {
+
+    private val viewModel: TestViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +26,36 @@ class WriteActivity : BindingActivity<ActivityWriteBinding>(R.layout.activity_wr
         binding.clEmpty.visibility = View.VISIBLE
         //binding.clEmpty.visibility = View.INVISIBLE
         setClickListeners()
+
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.templateStateFlow.collect {
+                    render(it)
+                }
+            }
+        }// 마찬가지로 코루틴 열고 수집을 하는데 생명주기에 맞춰서 flow가 자동으로
+        // 꺼지고 수집하고 될수 있도록 repeatOnLifeCycle이란걸 사용! 그리고
+        // 뷰모델에 있는 StateFlow에서 뷰모델에서 해줬던것처럼 collect 해준다/
+        // 여기 내부 값은 UiState가 들어오게 된다!
+
     }
+
+
+    private fun render(uiState: UiState) {
+        // 실제로 뷰에서 대응하는 함수 프로그래스바  visibility조절, 에러메시지 출력등을 하면 된다!
+        when (uiState) {
+            is UiState.Loading -> {//TODO
+                }
+            is UiState.Success<*> -> {
+                Timber.e(uiState.data.toString())
+            }
+            is UiState.Error -> {
+                Timber.e(uiState.message)
+            }
+        }
+    }
+
 
     private fun setClickListeners() {
         binding.apply {
