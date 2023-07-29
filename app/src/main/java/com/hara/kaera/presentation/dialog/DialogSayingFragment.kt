d8dcecdaca90bc1289230b7fe3a74e329dba745f
junwoo <rhffldkt0517@naver.com>
2023-07-29 18:18:04 +0900

    [Ui] 명언 다이얼로그 뷰 구성

--- app/src/main/java/com/hara/kaera/presentation/dialog/DialogSayingFragment.kt
+++ app/src/main/java/com/hara/kaera/presentation/dialog/DialogSayingFragment.kt
@@ -3,11 +3,11 @@ package com.hara.kaera.presentation.dialog
 import android.os.Bundle
 import android.view.View
 import com.hara.kaera.R
-import com.hara.kaera.databinding.DialogFragmentEditBinding
+import com.hara.kaera.databinding.DialogFragmentSayingBinding
 import com.hara.kaera.presentation.base.BindingDialogFragment
 
-class DialogEditFragment :
-    BindingDialogFragment<DialogFragmentEditBinding>(R.layout.dialog_fragment_edit, 24) {
+class DialogSayingFragment :
+    BindingDialogFragment<DialogFragmentSayingBinding>(R.layout.dialog_fragment_saying, 20) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
