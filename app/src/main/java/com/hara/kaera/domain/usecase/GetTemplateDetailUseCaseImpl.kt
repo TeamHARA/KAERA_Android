5567707663586061f2ff67e650e27f067c6d57f7
IslandofDream <rhffldkt0517@naver.com>
2023-08-07 00:31:25 +0900

    [Fix] flow를 반환해주지 않아 서버통신이 되지 않던 에러 해결

--- app/src/main/java/com/hara/kaera/domain/usecase/GetTemplateDetailUseCaseImpl.kt
+++ app/src/main/java/com/hara/kaera/domain/usecase/GetTemplateDetailUseCaseImpl.kt
@@ -3,14 +3,10 @@ package com.hara.kaera.domain.usecase
 import com.hara.kaera.domain.entity.TemplateDetailEntity
 import com.hara.kaera.domain.repository.KaeraRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import timber.log.Timber
 import javax.inject.Inject
 
 class GetTemplateDetailUseCaseImpl  @Inject constructor(private val repository: KaeraRepository) :GetTemplateDetailUseCase{
     override fun getTemplateDetailFlow(templateId : Int): Flow<TemplateDetailEntity> {
-        return flow {
-            repository.getTemplateDetailInfo(templateId)
-        }
+        return repository.getTemplateDetailInfo(templateId)
     }
 }
