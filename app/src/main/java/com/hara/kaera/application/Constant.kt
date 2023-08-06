44e795f4be087031fa46b6a4c8c01c06c28c9c65
IslandofDream <rhffldkt0517@naver.com>
2023-08-07 00:32:30 +0900

    [Refactor] Bearer 토큰 로컬프로퍼티로 분리

--- app/src/main/java/com/hara/kaera/application/Constant.kt
+++ app/src/main/java/com/hara/kaera/application/Constant.kt
@@ -2,5 +2,4 @@ package com.hara.kaera.application
 
 object Constant {
     const val APPLICATION_JSON = "application/json"
-    const val BEARER_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjExLCJpYXQiOjE2OTA4MTc4ODEsImV4cCI6MTY5ODU5Mzg4MX0.qTIr-AeF-0qx7HmoQFSvF0YJ2UdkSaTo_dX3Xgjqlj8"
 }
