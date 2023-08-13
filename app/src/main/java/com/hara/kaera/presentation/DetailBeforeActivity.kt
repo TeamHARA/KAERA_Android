785c199eecc7e0466cd567f80cb6c2cf0833ae99
Skylar <twinkle991@naver.com>
2023-08-13 20:24:46 +0900

    [Temp] UI 만드는 중

---
+++ app/src/main/java/com/hara/kaera/presentation/DetailBeforeActivity.kt
@@ -0,0 +1,13 @@
+package com.hara.kaera.presentation
+
+import android.os.Bundle
+import com.hara.kaera.R
+import com.hara.kaera.databinding.ActivityDetailBeforeBinding
+import com.hara.kaera.presentation.base.BindingActivity
+
+class DetailBeforeActivity :
+    BindingActivity<ActivityDetailBeforeBinding>(R.layout.activity_detail_before) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
