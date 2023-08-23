3f86064a6c46dd5b24ddbea970351f66d8ab4919
IslandofDream <rhffldkt0517@naver.com>
2023-08-23 17:06:00 +0900

    [Feat] 에러코드 및 에러타입 재정의

---
+++ app/src/main/java/com/hara/kaera/data/util/ErrorCode.kt
@@ -0,0 +1,17 @@
+package com.hara.kaera.data.util
+
+object ErrorCodes {
+
+    object Client {
+        const val BadRequest = 400
+        const val UnAuthorized = 401
+        const val Forbidden = 403
+        const val ResourceNotFound = 404
+        const val MethodNotAllowed = 405
+    }
+    object Server{
+        const val InternalServer = 500
+        const val ServiceUnavailable = 503
+        const val GateWayTimeOut = 504
+    }
+}
--- app/src/main/java/com/hara/kaera/data/util/ErrorType.kt
+++ app/src/main/java/com/hara/kaera/data/util/ErrorType.kt
@@ -2,19 +2,23 @@ package com.hara.kaera.data.util
 
 sealed class ErrorType {
 
-    sealed class Api: ErrorType() {
-
-        object Network: Api()
-
+    sealed class Api : ErrorType() {
+        object BadRequest : Api()
+        object UnAuthorized : Api()
+        object Forbidden : Api()
+        object ResourceNotFound : Api()
+        object MethodNotAllowed : Api()
+        object InternalServer : Api()
         object ServiceUnavailable : Api()
-
-        object NotFound : Api()
-
-        object Server : Api()
+        object GateWayTimeOut : Api()
 
     }
+    object IoException :ErrorType()
+    object Network : ErrorType()
+    object Socket : ErrorType()
 
-    object Unknown: ErrorType()
+    object Unknown : ErrorType()
 
-    // other categories of Error
+
+
 }
