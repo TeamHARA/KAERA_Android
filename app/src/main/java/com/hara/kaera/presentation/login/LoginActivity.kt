51ebd56218443175509399b99f2c0fe5b43643cc
IslandofDream <rhffldkt0517@naver.com>
2023-08-31 17:52:38 +0900

    [Feat] 로그인 액티비티 추가

---
+++ app/src/main/java/com/hara/kaera/presentation/login/LoginActivity.kt
@@ -0,0 +1,9 @@
+package com.hara.kaera.presentation.login
+
+import com.hara.kaera.R
+import com.hara.kaera.databinding.ActivityLoginBinding
+import com.hara.kaera.presentation.base.BindingActivity
+
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
+
+}
