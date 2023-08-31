58777e3eb57a2dd321571e8a92157fa75c922e62
IslandofDream <rhffldkt0517@naver.com>
2023-08-31 23:30:07 +0900

    [Feat] 카카오 유저 클라이언트 주입 및 콜백함수 코루틴화

---
+++ app/src/main/java/com/hara/kaera/data/datasource/KaKaoLoginClient.kt
@@ -0,0 +1,67 @@
+package com.hara.kaera.data.datasource
+
+import android.content.Context
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
+import dagger.hilt.android.qualifiers.ActivityContext
+import javax.inject.Inject
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+class KaKaoLoginClient @Inject constructor(
+    @ActivityContext context: Context,
+    private val userApiClient: UserApiClient
+) {
+
+
+    suspend fun UserApiClient.Companion.login(context: Context): Result<OAuthToken> = runCatching {
+        if (userApiClient.isKakaoTalkLoginAvailable(context)) { // 카카오톡이 설치되어있는가?
+            try {
+                UserApiClient.loginWithKakaoTalk(context)
+            } catch (error: Throwable) {
+                if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                    // 사용자가 뒤로가기 선택으로 의도적 취소
+                    throw error
+                } else {
+                    UserApiClient.loginWithKakaoAccount(context)
+                }
+            }
+        } else { // 카카오계정(웹)으로 로그인
+            UserApiClient.loginWithKakaoAccount(context)
+        }
+    }
+
+    // 카카오톡으로 로그인 시도
+    private suspend fun UserApiClient.Companion.loginWithKakaoTalk(context: Context): OAuthToken =
+        suspendCoroutine { continuation ->
+            instance.loginWithKakaoTalk(context) { token, error ->
+                continuation.resumeTokenOrException(token, error)
+            }
+        }
+
+    // 카카오 계정으로 로그인 시도
+    private suspend fun UserApiClient.Companion.loginWithKakaoAccount(context: Context): OAuthToken =
+        suspendCoroutine { continuation ->
+            instance.loginWithKakaoAccount(context) { token, error ->
+                continuation.resumeTokenOrException(token, error)
+            }
+        }
+
+    //
+
+    private fun Continuation<OAuthToken>.resumeTokenOrException(token: OAuthToken?, error: Throwable?) {
+        if (error != null) {
+            resumeWithException(error)
+        } else if (token != null) {
+            resume(token)
+        } else {
+            resumeWithException(RuntimeException("토큰 접근 에러"))
+        }
+    }
+
+
+}
