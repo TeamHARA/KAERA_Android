71ee4e941500774ba71d892a83f42e54ef9cba2b
Yujin <69359774+wkdyujin@users.noreply.github.com>
2023-09-17 23:28:25 +0900

    [Feat] 마이페이지 UI 구현 - 토글 색상이 바뀌지 않는 이슈 있음..

---
+++ app/src/main/java/com/hara/kaera/feature/mypage/MypageActivity.kt
@@ -0,0 +1,12 @@
+package com.hara.kaera.feature.mypage
+
+import android.os.Bundle
+import com.hara.kaera.R
+import com.hara.kaera.databinding.ActivityMypageBinding
+import com.hara.kaera.feature.base.BindingActivity
+
+class MypageActivity : BindingActivity<ActivityMypageBinding>(R.layout.activity_mypage) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
