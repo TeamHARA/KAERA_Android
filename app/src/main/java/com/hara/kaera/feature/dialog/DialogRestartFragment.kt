c45f753a321f2d80382ed77b8f29fe4e96481e94
IslandofDream <rhffldkt0517@naver.com>
2023-11-04 13:58:19 +0900

    [Feat] 다이얼로그 상황에서 뒤로가기 처리 추가

--- app/src/main/java/com/hara/kaera/feature/dialog/DialogRestartFragment.kt
+++ app/src/main/java/com/hara/kaera/feature/dialog/DialogRestartFragment.kt
@@ -1,11 +1,13 @@
 package com.hara.kaera.feature.dialog
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
 import com.hara.kaera.R
 import com.hara.kaera.databinding.DialogFragmentRestartBinding
-import com.hara.kaera.databinding.DialogFragmentSayingBinding
 import com.hara.kaera.feature.base.BindingDialogFragment
+
 
 class DialogRestartFragment(
     private val clickListener: () -> Unit,
@@ -18,6 +20,14 @@ class DialogRestartFragment(
         binding.btnRestart.setOnClickListener {
             clickListener.invoke()
             dismiss()
+        }
+        // 뒤로가기의 경우 activity를 종료
+        dialog!!.setOnKeyListener { dialog: DialogInterface?, keyCode: Int, event: KeyEvent ->
+            if (keyCode == KeyEvent.KEYCODE_BACK && event.action == KeyEvent.ACTION_UP) {
+                requireActivity().finish()
+                return@setOnKeyListener true
+            }
+            false
         }
     }
 
