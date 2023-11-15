4964bfab97b4fd0d13f93673164a4edfb7b862b5
IslandofDream <rhffldkt0517@naver.com>
2023-11-15 22:34:11 +0900

    [Feat] 에러화면 적용 및 템플릿 아이디 상수화

--- app/src/main/java/com/hara/kaera/feature/util/Constant.kt
+++ app/src/main/java/com/hara/kaera/feature/util/Constant.kt
@@ -4,4 +4,6 @@ object Constant {
     val homeGemsSequence = arrayOf(0, 4, 9, 1, 8, 11, 3, 7, 5, 2, 10, 6)
     const val networkError = "네트워크 에러"
     const val internalError = "앱/웹 에러"
+    const val freeNoteId = 1
+    const val thanksToId = 5
 }
