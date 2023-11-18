4d2eb2e80792a48bbc2b9ab188a8d5a2c93030f9
Yujin <69359774+wkdyujin@users.noreply.github.com>
2023-11-18 15:52:36 +0900

    [Fix] 고민 템플릿 스크롤 범위 수정 - 인스타그램 공유 배너까지 스크롤 가능하게 수정 - 멀티 뷰 홀더 사용

--- app/src/main/java/com/hara/kaera/feature/storage/worrytemplate/WorryTemplateAdapter.kt
+++ app/src/main/java/com/hara/kaera/feature/storage/worrytemplate/WorryTemplateAdapter.kt
@@ -5,33 +5,76 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.hara.kaera.databinding.ItemWorryTemplateBinding
+import com.hara.kaera.databinding.LayoutWorryTemplateBannerBinding
 import com.hara.kaera.domain.entity.TemplateTypesEntity
 import com.hara.kaera.feature.util.GlobalDiffCallBack
+import timber.log.Timber
 
-class WorryTemplateAdapter() :
-    ListAdapter<TemplateTypesEntity.Template, WorryTemplateAdapter.ItemViewHolder>(GlobalDiffCallBack()) {
+class WorryTemplateAdapter(private val itemClickListener: (Int) -> Unit) :
+    ListAdapter<TemplateTypesEntity.Template, RecyclerView.ViewHolder>(
+        GlobalDiffCallBack(),
+    ) {
     private lateinit var inflater: LayoutInflater
+
+    class BannerViewHolder(val binding: LayoutWorryTemplateBannerBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     class ItemViewHolder(val binding: ItemWorryTemplateBinding) :
         RecyclerView.ViewHolder(binding.root)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 0) TYPE_BANNER else TYPE_TEMPLATE
+    }
+
+    override fun getItemCount() = super.getItemCount() + 1
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         if (!::inflater.isInitialized) {
             inflater = LayoutInflater.from(parent.context)
         }
-        return ItemViewHolder(ItemWorryTemplateBinding.inflate(inflater, parent, false))
+        return when (viewType) {
+            TYPE_BANNER -> BannerViewHolder(
+                LayoutWorryTemplateBannerBinding.inflate(
+                    inflater,
+                    parent,
+                    false,
+                ),
+            )
+
+            else -> ItemViewHolder(ItemWorryTemplateBinding.inflate(inflater, parent, false))
+        }
     }
 
-    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val curItem = getItem(position)
-        with(holder.binding) {
-            this.template = curItem
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is BannerViewHolder -> {
+                holder.binding.btnShare.setOnClickListener {
+                    Timber.e("Click Share Button")
+                    // TODO: 인스타그램 공유
+                }
+            }
 
-            ivArrow.setOnClickListener {
-                val currentState = this.isExpand ?: false
-                ExpandAnimation.toggleArrow(ivArrow, !currentState)
-                this.isExpand = !currentState
+            is ItemViewHolder -> {
+                val curItem = getItem(position - 1)
+                with(holder.binding) {
+                    this.template = curItem
+
+                    this.clTemplateTitle.setOnClickListener {
+                        val currentState = this.isExpand ?: false
+                        ExpandAnimation.toggleArrow(ivArrow, !currentState)
+                        this.isExpand = !currentState
+                    }
+
+                    this.btnWrite.setOnClickListener {
+                        itemClickListener(curItem.templateId)
+                    }
+                }
             }
         }
+    }
+
+    companion object {
+        private const val TYPE_BANNER = 0
+        private const val TYPE_TEMPLATE = 1
     }
 }
