2f6eda9fa702558af34dbf7058708e83344dd106
junwoo <rhffldkt0517@naver.com>
2023-11-22 17:46:51 +0900

    [Fix] 401 코드가 반환될 경우 다시 토큰 재발급을 거치고 새로 request하도록 interceptor 추가

--- app/src/main/java/com/hara/kaera/di/ServiceModule.kt
+++ app/src/main/java/com/hara/kaera/di/ServiceModule.kt
@@ -4,7 +4,9 @@ import com.hara.kaera.BuildConfig
 import com.hara.kaera.application.Constant
 import com.hara.kaera.data.datasource.remote.KaeraApi
 import com.hara.kaera.data.datasource.remote.LoginApi
+import com.hara.kaera.data.datasource.remote.LoginDataSource
 import com.hara.kaera.data.util.ErrorHandlerImpl
+import com.hara.kaera.domain.dto.JWTRefreshReqDTO
 import com.hara.kaera.domain.repository.LoginRepository
 import com.hara.kaera.domain.util.ErrorHandler
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
@@ -12,7 +14,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
@@ -21,7 +26,6 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import timber.log.Timber
 import javax.inject.Qualifier
 import javax.inject.Singleton
 
@@ -38,19 +42,54 @@ object ServiceModule {
     @Singleton
     @KAREARetrofit
     fun providesKaeraHeaderInterceptor(
-        loginRepositroy: LoginRepository
+        loginRepository: LoginRepository,
+        loginDataStore: LoginDataSource
     ): Interceptor {
+
         val authToken: String = runBlocking {
-            loginRepositroy.getSavedAccessToken().first()
+            loginRepository.getSavedAccessToken().first()
         }
+
         return Interceptor { chain ->
-            with(chain) {
-                val request = request().newBuilder()
+            var request = chain.request().newBuilder()
+                .addHeader("Accept", Constant.APPLICATION_JSON)
+                //.addHeader("Authorization", authToken ?: "nothing")
+                .addHeader("Authorization", BuildConfig.BEARER_TOKEN)
+                .build()
+
+            if (chain.proceed(request).code == 401) {
+                val refreshToken: String = runBlocking {
+                    loginRepository.getSavedRefreshToken().first()
+                }
+                CoroutineScope(Dispatchers.IO).launch {
+                    kotlin.runCatching {
+                        loginDataStore.getAccessToken(
+                            JWTRefreshReqDTO(
+                                accessToken = authToken,
+                                refreshToken = refreshToken
+                            )
+                        )
+                    }
+                        .onSuccess {
+                            it.collect {
+                                loginRepository.updateAccessToken(accessToken = it.data.accessToken)
+                            }
+                        }.onFailure {
+                            throw it
+                        }
+                }
+
+                val newAuthToken: String = runBlocking {
+                    loginRepository.getSavedAccessToken().first()
+                }
+
+                val newRequest = chain.request().newBuilder()
                     .addHeader("Accept", Constant.APPLICATION_JSON)
-                    .addHeader("Authorization", authToken)
-                    //.addHeader("Authorization", BuildConfig.BEARER_TOKEN)
+                    .addHeader("Authorization", newAuthToken ?: "nothing")
                     .build()
-                proceed(request)
+                chain.proceed(newRequest)
+            }else{
+                chain.proceed(request)
             }
         }
     }
