71fabc4800030425a1aa49173977c6ac5ba19904
IslandofDream <rhffldkt0517@naver.com>
2023-11-27 21:57:45 +0900

    [FIX] 다이얼로그 부정 선택 버튼 텍스트 색 변경

--- app/src/main/java/com/hara/kaera/feature/detail/custom/DialogUpdateWarning.kt
+++ app/src/main/java/com/hara/kaera/feature/detail/custom/DialogUpdateWarning.kt
@@ -3,7 +3,7 @@ package com.hara.kaera.feature.detail.custom
 import android.os.Bundle
 import android.view.View
 import com.hara.kaera.R
-import com.hara.kaera.databinding.DialogWarningBinding
+import com.hara.kaera.databinding.DialogFragmentTemplatechangeWarningBinding
 import com.hara.kaera.feature.base.BindingDialogFragment
 import com.hara.kaera.feature.util.onSingleClick
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,7 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class DialogUpdateWarning(
     private val yesClickListener: () -> Unit,
-) : BindingDialogFragment<DialogWarningBinding>(R.layout.dialog_warning, 16) {
+) : BindingDialogFragment<DialogFragmentTemplatechangeWarningBinding>(R.layout.dialog_fragment_templatechange_warning, 16) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setListeners()
