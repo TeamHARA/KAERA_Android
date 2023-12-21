c116c05227c5ef76b023640fc8b253610d3d63f2
IslandofDream <rhffldkt0517@naver.com>
2023-12-22 00:52:29 +0900

    [Refactor] 메인액티비티 알림 권한 플로우 수정

--- app/src/main/java/com/hara/kaera/feature/MainActivity.kt
+++ app/src/main/java/com/hara/kaera/feature/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.hara.kaera.feature
 
+import android.Manifest
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import com.hara.kaera.R
@@ -14,7 +19,6 @@ import com.hara.kaera.feature.home.HomeFragment
 import com.hara.kaera.feature.home.HomeViewModel
 import com.hara.kaera.feature.storage.StorageFragment
 import com.hara.kaera.feature.util.Constant
-import com.hara.kaera.feature.util.PermissionRequestDelegator
 import com.hara.kaera.feature.util.makeToast
 import com.hara.kaera.feature.util.navigateTo
 import com.hara.kaera.feature.util.stringOf
@@ -29,12 +33,13 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     private val viewModel by viewModels<HomeViewModel>()
 
 
+    private lateinit var launcher: ActivityResultLauncher<Array<String>>
+
     private var time: Long = 0
     private val callback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
             if (System.currentTimeMillis() - time >= 2000) {
                 time = System.currentTimeMillis()
-                Timber.e("home backpress")
                 KaeraSnackBar.make(
                     view = binding.root,
                     message = baseContext.stringOf(R.string.main_backpress),
@@ -50,15 +55,54 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (PermissionRequestDelegator(this).checkPermissions(PermissionRequestDelegator.PLACE.HOME) == true) {
-            binding.root.makeToast("원활한 서비스를 위해서 알림을 활성화 해 주세요!")
-        }
+        setPermission()
         this.onBackPressedDispatcher.addCallback(this, callback)
         //TODO 원석이 가득찰 경우 DialogFullStoneFragment().show(supportFragmentManager, "full_stone")
         //이거 수정하려면 HomeFragment에 있는 뷰모델을 액티비티에서 생성되도록 옮기고
         // 내부 아이템이 12면 바텀내비 부분에서 조건문으로 분기처리 해줘야 되겠네
         registerBottomNav()
     }
+
+    private fun setPermission() {
+
+        launcher =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                val deniedPermissionList = permissions.filter { !it.value }.map { it.key }
+                when {
+                    deniedPermissionList.isNotEmpty() -> {
+                        val map = deniedPermissionList.groupBy { permission ->
+                            if (shouldShowRequestPermissionRationale(permission)) "denied" else "explained"
+                        }
+                        map["denied"]?.let {
+                            // 최초거절 케이스 (앱 최초 설치이후 한번만 타게 됨)
+                            binding.root.makeToast("원활한 서비스 이용을 위해서 알림권한을 허용해주세요!")
+                        }
+                        map["explained"]?.let {
+                            //권한 영구 거절( 2번째 거절 이후 ) 메인에서는 아무런 동작도 안함
+                        }
+                    }
+
+                    else -> Unit
+                }
+            }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                Timber.e("ration")
+
+                //TODO rationale dialog
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    1
+                )
+            } else {
+                launcher.launch(arrayOf(Manifest.permission.POST_NOTIFICATIONS))
+            }
+
+        }
+    }
+
 
     private fun registerBottomNav() {
         with(binding.bottomNav) {
