1a3953f65df236c9a308fd61776582a99767df37
IslandofDream <rhffldkt0517@naver.com>
2023-12-24 10:48:48 +0900

    [Feat] 릴리즈 버전 디버그 방지 / 깃 이그노어 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 
     buildTypes {
         release {
+            isDebuggable = false
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
